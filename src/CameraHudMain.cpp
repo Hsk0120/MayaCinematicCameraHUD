bfa611199e8858b532141757c5070a695ca2e129
Hsk0120 <50475076+Hsk0120@users.noreply.github.com>
2025-12-06 14:45:39 +0900

    ソース公開用に移植

---
+++ src/CameraHudMain.cpp
@@ -0,0 +1,307 @@
+#include <maya/MFnDagNode.h>
+#include <iostream>
+#include <string>
+#include <maya/MFrameContext.h>
+#include <maya/M3dView.h>
+#include "CameraHudManager.h"
+#include "CameraHudUtility.h"
+
+//MObject CameraHudManager::aText;
+MObject CameraHudManager::aTextBoxTransparency;
+MObject CameraHudManager::aTextFontSize;
+
+MTypeId CameraHudManager::id(0x0013c801);
+MString CameraHudManager::drawDbClassification("drawdb/geometry/CameraHud");
+MString CameraHudManager::drawRegistrantId("CameraHudPlugin");
+
+// お作法
+CameraHudManager::CameraHudManager()
+{
+
+};
+
+CameraHudManager::~CameraHudManager()
+{
+
+};
+
+void* CameraHudManager::creator()
+{
+    return new CameraHudManager();
+};
+
+MStringArray CameraHudManagerData::fFontList;
+
+CameraHudManagerData::CameraHudManagerData()
+{
+
+};
+
+CameraHudManagerDrawOverride::CameraHudManagerDrawOverride(const MObject& obj)
+    : MPxDrawOverride(obj, NULL)
+{
+
+};
+
+CameraHudManagerDrawOverride::~CameraHudManagerDrawOverride()
+{
+
+};
+
+DrawAPI CameraHudManagerDrawOverride::supportedDrawAPIs() const
+{
+    return (kOpenGL | kDirectX11 | kOpenGLCoreProfile);
+};
+
+bool CameraHudManagerDrawOverride::isBounded(const MDagPath& objPath, const MDagPath& cameraPath) const
+{
+    return false;
+};
+
+MBoundingBox CameraHudManagerDrawOverride::boundingBox(const MDagPath& objPath, const MDagPath& cameraPath) const
+{
+    return MBoundingBox();
+};
+
+//  データを取得してキャッシュ
+MUserData* CameraHudManagerDrawOverride::prepareForDraw(const MDagPath& objPath, const MDagPath& cameraPath, const MHWRender::MFrameContext& frameContext, MUserData* oldData)
+{
+    CameraHudManagerData* data = dynamic_cast<CameraHudManagerData*>(oldData);
+    if (!data) {
+        data = new CameraHudManagerData();
+    };
+
+    MStatus status;
+    MObject CameraHudNode = objPath.node(&status);
+    if (status) {
+        //タイムコードをセット
+        data->fTimeCode = get_current_timecode();
+
+        //カレントフレームをセット
+        data->fFrame = get_frame("Current : ", get_current_frame());
+
+        //スタートフレームをセット
+        data->fStartFrame = get_frame("Start : ", get_start_frame());
+
+        //エンドフレームをセット
+        data->fEndFrame = get_frame("End : ", get_end_frame());
+
+        //アトリビュートテキストをセット
+        //data->fText = MPlug(CameraHudNode, CameraHudManager::aText).asString();
+
+        // カメラネームを取得してセット
+        data->fCamera = get_camera_name(MFnDagNode(frameContext.getCurrentCameraPath(), &status).name().asChar());
+
+        // カメラの焦点距離を取得してセット
+        data->fFocalLength = get_camera_focalLength(MFnCamera(MFnDagNode(frameContext.getCurrentCameraPath(), &status).object()).focalLength());
+
+
+        // カメラのキャッシュ状況を取得してセット
+        bool camera_cache_bool = MFnDagNode(frameContext.getCurrentCameraPath(), &status).findPlug(MString("camera_cache")).asBool();
+        //bool camera_cache_bool = true;
+        data->fCameraCache = get_camera_cache(camera_cache_bool);
+
+        // カットナンバーを取得してセット
+        data->fCut = get_cut_num(frameContext.getCurrentCameraPath());
+
+        // テキストサイズ取得
+        MPlug plugTextFontSize(CameraHudNode, CameraHudManager::aTextFontSize);
+        data->fTextFontSize = plugTextFontSize.asFloat();
+
+        // テキストボックスの透明度取得
+        MPlug plugTextBoxTransparency(CameraHudNode, CameraHudManager::aTextBoxTransparency);
+        data->fTextBoxColor.a = 1.0f - plugTextBoxTransparency.asFloat();
+
+
+        // ビューポートサイズ取得
+        int a, b, width, height;
+        frameContext.getViewportDimensions(a, b, width, height);
+
+
+
+        // カメラのオーバースキャンを取得して、ゲートマスクオフセットの計算
+        double overscan = MFnCamera(MFnDagNode(frameContext.getCurrentCameraPath(), &status).object()).overscan();
+        if(overscan != 1){
+            MString filmFit = getFilmFitAsString(MFnCamera(MFnDagNode(frameContext.getCurrentCameraPath(), &status).object()).filmFit());
+            if (filmFit == "fill") {
+                data->fMaskOffsetPosition = get_camera_resolution_fill(overscan, width, height);
+            }
+            else {
+                data->fMaskOffsetPosition = get_camera_resolution_horizontal(overscan, width, height);
+            }
+
+        }
+        else{
+            data->fMaskOffsetPosition = MPoint(0,0,0);
+        }
+
+        // ビューポートサイズからフォントサイズを変更
+        data->fTextFontSize = data->fTextFontSize * (height - data->fMaskOffsetPosition.y * 2) * 0.05 * 0.7;
+        data->fMarginPosition = MPoint(data->fTextFontSize*0.1, (height - data->fMaskOffsetPosition.y * 2) * 0.08 * 0.7, 0);
+        data->fOffsetPosition = MPoint(width * 0.01, (height - data->fMaskOffsetPosition.y * 2) * 0.005 * 0.7, 0);
+
+        // 各テキストのポジションを設定
+        //data->fPosition             = get_viewport_centerTop(data->fNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        
+        data->fStartFramePosition   = get_viewport_leftBottom(data->fStartFrameNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        data->fEndFramePosition     = get_viewport_leftBottom(data->fEndFrameNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        data->fFramePosition        = get_viewport_leftBottom(data->fFrameNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        
+        data->fCameraPosition       = get_viewport_leftTop(data->fCameraNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        data->fFocalLengthPosition  = get_viewport_leftTop(data->fFocalLengthNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        data->fCutLengthPosition    = get_viewport_leftTop(data->fCutLengthNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        data->fCameraCachePosition  = get_viewport_leftTop(data->fCameraCacheNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+
+
+        data->fTimeCodePosition     = get_viewport_rightBottom(data->fTimeCodeNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        
+        data->fCurrentDatePosition  = get_viewport_rightTop(data->fCurrentDateNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+        data->fUserNamePosition     = get_viewport_rightTop(data->fUserNameNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+
+        data->fSceneNamePosition    = get_viewport_centerBottom(data->fSceneNameNum, width, height, data->fTextFontSize, data->fOffsetPosition, data->fMarginPosition, data->fMaskOffsetPosition);
+    
+    };
+    return data;
+};
+
+// prepareForDraw()の直後に実行　MUIDrawManagerへのアクセスを提供
+void CameraHudManagerDrawOverride::addUIDrawables(const MDagPath& objPath, MHWRender::MUIDrawManager& drawManager, const MHWRender::MFrameContext& frameContext, const MUserData* data)
+{
+    const CameraHudManagerData* thisdata = dynamic_cast<const CameraHudManagerData*>(data);
+    if (!thisdata) {
+        return;
+    }
+
+    // UI表示処理開始
+    drawManager.beginDrawable();
+
+    // テスト　デプス確認
+    drawManager.setDepthPriority(100);
+
+
+    // テキストのスタイルをセット
+    drawManager.setColor(thisdata->fColor);
+    drawManager.setFontSize(thisdata->fTextFontSize);
+    drawManager.setFontIncline(thisdata->fTextIncline);
+    drawManager.setFontWeight(thisdata->fTextWeight);
+    drawManager.setFontName(thisdata->fFontFaceName);
+    int boxSize[] = { thisdata->fTextBoxWidth, thisdata->fTextBoxHeight };
+
+    // アトリビュートテキスト表示
+    //drawManager.text2d(thisdata->fPosition, thisdata->fText, thisdata->fTextAlignment,
+    //    boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // カメラ名表示
+    drawManager.text2d(thisdata->fCameraPosition, thisdata->fCamera, thisdata->fCameraTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // 焦点距離表示
+    drawManager.text2d(thisdata->fFocalLengthPosition, thisdata->fFocalLength, thisdata->fFocalLengthTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // カットナンバーを表示
+    drawManager.text2d(thisdata->fCutLengthPosition, thisdata->fCut, thisdata->fCutLengthTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // カメラキャッシュ表示
+    drawManager.text2d(thisdata->fCameraCachePosition, thisdata->fCameraCache, thisdata->fCameraCacheTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // タイムコード表示
+    drawManager.text2d(thisdata->fTimeCodePosition, thisdata->fTimeCode, thisdata->fTimeCodeTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // カレントフレーム表示
+    drawManager.text2d(thisdata->fFramePosition, thisdata->fFrame, thisdata->fFrameTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    //  スタートフレーム表示
+    drawManager.text2d(thisdata->fStartFramePosition, thisdata->fStartFrame, thisdata->fStartFrameTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // エンドフレーム表示
+    drawManager.text2d(thisdata->fEndFramePosition, thisdata->fEndFrame, thisdata->fEndFrameTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // ユーザー名表示
+    drawManager.text2d(thisdata->fUserNamePosition, thisdata->fUserName, thisdata->fUserNameTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // シーン名表示
+    drawManager.text2d(thisdata->fSceneNamePosition, thisdata->fSceneName, thisdata->fSceneNameTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+    // 今日の日付を表示
+    drawManager.text2d(thisdata->fCurrentDatePosition, thisdata->fCurrentDate, thisdata->fCurrentDateTextAlignment,
+        boxSize[0] + boxSize[1] == 0 ? NULL : boxSize, &thisdata->fTextBoxColor, false);
+
+    drawManager.endDrawable();
+
+};
+
+// ノードのアトリビュート登録処理
+MStatus CameraHudManager::initialize()
+{
+    MStatus status;
+    MFnNumericAttribute nAttr;
+    MFnEnumAttribute eAttr;
+    MFnTypedAttribute typedAttr;
+
+    // Add text attributes.
+    //aText = typedAttr.create("text", "t", MFnData::kString, MFnStringData().create("Text"));
+    //MPxNode::addAttribute(aText);
+
+    aTextFontSize = nAttr.create("textFontSize", "tfs", MFnNumericData::kFloat,     1.0);
+    nAttr.setMin(-1);
+    nAttr.setMax(99);
+    MPxNode::addAttribute(aTextFontSize);
+
+    aTextBoxTransparency = nAttr.create("textBoxTransparency", "tbt", MFnNumericData::kFloat, 0.3);
+    nAttr.setSoftMin(0.0);
+    nAttr.setSoftMax(1.0);
+    MPxNode::addAttribute(aTextBoxTransparency);
+
+    return MS::kSuccess;
+};
+
+// プラグインノードを登録
+MStatus initializePlugin(MObject obj) 
+{
+    MStatus   status;
+    MFnPlugin plugin(obj, "Tool", "1.0", "Any");
+
+    status = plugin.registerNode(
+        "CameraHud",
+        CameraHudManager::id,
+        &CameraHudManager::creator,
+        &CameraHudManager::initialize,
+        MPxNode::kLocatorNode,
+        &CameraHudManager::drawDbClassification);
+    if (!status) {
+        status.perror("registerNode");
+        return status;
+    }
+
+    status = MHWRender::MDrawRegistry::registerDrawOverrideCreator(
+        CameraHudManager::drawDbClassification,
+        CameraHudManager::drawRegistrantId,
+        CameraHudManagerDrawOverride::Creator);
+    if (!status) {
+        status.perror("registerDrawOverrideCreator");
+        return status;
+    }
+
+    return status;
+};
+
+//  プラグインノードを解除
+MStatus uninitializePlugin(MObject obj)  
+{
+    MStatus   status;
+    MFnPlugin plugin(obj);
+
+    status = MDrawRegistry::deregisterGeometryOverrideCreator(
+        CameraHudManager::drawDbClassification,
+        CameraHudManager::drawRegistrantId);
+    if (!status) {
+        status.perror("deregisterGeometryOverrideCreator");
+        return status;
+    }
+
+    status = plugin.deregisterNode(CameraHudManager::id);
+    if (!status) {
+        status.perror("deregisterNode");
+        return status;
+    }
+
+    return status;
+};
---
+++ src/CameraHudUtility.h
@@ -0,0 +1,392 @@
+#pragma once
+#include <maya/MGlobal.h>
+#include <maya/MString.h>
+#include <maya/MTime.h>
+#include <maya/MAnimControl.h>
+#include <maya/MGlobal.h>
+#include <windows.h>
+#include <string>
+#include <lmcons.h>
+#include <maya/MFileObject.h>
+#include <maya/MFileIO.h>
+#include <maya/MStringArray.h>
+#include <sstream>
+#include <iomanip>
+#include <chrono>
+#include <ctime>
+#include <maya/MFnDagNode.h>
+#include <maya/MFrameContext.h>
+#include <maya/MFnCamera.h>
+#include <maya/MFnDagNode.h>
+#include <maya/MDagPath.h>
+#include <cmath>
+#include <maya/MNamespace.h>
+#include <iostream>
+
+// 今日の日付を年/月/日 で取得する
+std::string get_current_date() {
+    // 現在の時刻を取得
+    auto now = std::chrono::system_clock::now();
+    std::time_t time_now = std::chrono::system_clock::to_time_t(now);
+
+    // tm構造体に変換
+    std::tm* time_info = std::localtime(&time_now);
+
+    // 必要な形式で出力
+    std::stringstream ss;
+    ss << std::put_time(time_info, "%Y/%m/%d");
+    return ss.str();
+}
+
+void convert_unitToTime(const MTime::Unit &unit, double &framesPerSecond)
+{
+	switch (unit) {
+	case MTime::kGames:
+		framesPerSecond = 15.0;
+		break;
+	case MTime::kFilm:
+		framesPerSecond = 24.0;
+		break;
+	case MTime::kPALFrame:
+		framesPerSecond = 25.0;
+		break;
+	case MTime::kNTSCFrame:
+		framesPerSecond = 30.0;
+		break;
+	case MTime::kShowScan:
+		framesPerSecond = 48.0;
+		break;
+	case MTime::kPALField:
+		framesPerSecond = 50.0;
+		break;
+	case MTime::kNTSCField:
+		framesPerSecond = 60.0;
+		break;
+	case MTime::k120FPS :
+		framesPerSecond = 120.0;
+		break;    
+	default:
+		framesPerSecond = 60.0;
+		break;
+	}
+}
+
+// 秒数とfpsから時間:分:秒:フレーム を取得
+void convert_secondsToTime(double seconds, int& hours, int& minutes, int& secondsOut, int& frames, double framesPerSecond, MTime::Unit unit) {
+    convert_unitToTime(unit, framesPerSecond);
+    
+    const int framesPerMinute = 60 * static_cast<int>(framesPerSecond);
+    const int framesPerHour = 60 * framesPerMinute;
+
+    // Compute the number of hours, minutes, and seconds
+    hours = static_cast<int>(std::floor(seconds / 3600.0));
+    seconds -= hours * 3600.0;
+    minutes = static_cast<int>(std::floor(seconds / 60.0));
+    seconds -= minutes * 60.0;
+    secondsOut = static_cast<int>(std::floor(seconds));
+    seconds -= secondsOut;
+    frames = static_cast<int>(std::round(seconds * framesPerSecond));
+
+    // Adjust for overflow in the frames count
+    if (frames >= static_cast<int>(framesPerSecond)) {
+        frames = 0;
+        ++secondsOut;
+    }
+
+    if (secondsOut >= 60) {
+        secondsOut = 0;
+        ++minutes;
+    }
+
+    if (minutes >= 60) {
+        minutes = 0;
+        ++hours;
+    }
+}
+
+// タイムコード取得
+MString get_current_timecode() {
+    MTime currentTime = MAnimControl::currentTime();
+    double currentSeconds = currentTime.as(MTime::kSeconds);
+
+    int hours, minutes, seconds, frames;
+    double framesPerSecond = 60.0;
+ 
+    MTime::Unit unit = currentTime.unit();
+    convert_secondsToTime(currentSeconds, hours, minutes, seconds, frames, framesPerSecond, unit);
+
+    std::stringstream ss;
+    ss << std::setfill('0') << std::setw(2) << hours << ":"
+        << std::setfill('0') << std::setw(2) << minutes << ":"
+        << std::setfill('0') << std::setw(2) << seconds << ":"
+        << std::setfill('0') << std::setw(2) << frames;
+
+    return MString(ss.str().c_str());
+}
+
+// カレントフレームを取得する関数
+double get_current_frame()
+{
+    MTime currentTime = MAnimControl::currentTime();
+    double currentFrame = currentTime.as(currentTime.unit());
+    return currentFrame; // 取得に失敗した場合は0を返す
+}
+
+// スタートフレーム取得
+double get_start_frame()
+{
+    MTime startTime = MAnimControl::minTime();
+    double startFrame = startTime.as(startTime.unit());
+    return startFrame; 
+}
+
+// エンドフレームを取得
+double get_end_frame()
+{
+    MTime endTime = MAnimControl::maxTime();
+    double endFrame = endTime.as(endTime.unit());
+    return endFrame;
+}
+
+// doubleからcharに変換
+void double_to_char(double value, char* str, int precision)
+{
+    sprintf(str, "%.*f", precision, value);
+}
+
+// フレーム数の取得と+fの文字列足し
+char* get_frame(static char result[64], double frame)
+{
+    static char str[64];
+    static char num[64];
+    double_to_char(frame, num, 0);
+    strcpy(str, result);
+    strcat(str, num); 
+    strcat(str, "f"); // "f"を結合する
+    return str;
+}
+
+// ユーザー名を取得
+std::string get_username() {
+    char username[UNLEN + 1];
+    DWORD size = sizeof(username);
+
+    if (GetUserNameA(username, &size)) {
+        return std::string(username);
+    }
+    else {
+        return "";
+    }
+}
+
+// シーン名取得
+MString get_scene_name() {
+    MString fileObj = MFileIO::currentFile();
+    MStringArray nameArray;
+    fileObj.split('/', nameArray);
+    return nameArray[nameArray.length() - 1];
+}
+
+// カメラネームからShape削除
+MString get_camera_name(std::string camName){
+    std::size_t pos = camName.find("Shape");  // "shape" の位置を検索
+    if (pos != std::string::npos) {  // "shape" が見つかった場合
+        camName.erase(pos);  // "shape" 以降の文字列を削除
+    }
+    return MString(camName.data());
+}
+
+MString GetNamespaceFromObject(const MDagPath& dagPath)
+{
+    MStatus status;
+    MString namespaceObj = MNamespace::getNamespaceFromName(dagPath.partialPathName(), &status);
+
+    // 文字列の長さを取得
+    int length = namespaceObj.length();
+
+    // 最後の3文字の範囲を指定して取得
+    MString cut_number = namespaceObj.substring(length - 3, length);
+    cut_number = MString("Cut:") + cut_number;
+    return cut_number;
+}
+
+// カットナンバー取得
+MString get_cut_num(const MDagPath &camDagPath) {
+    MString namespaceName = GetNamespaceFromObject(camDagPath);
+    return namespaceName;
+}
+
+// カメラの焦点距離を取得
+MString get_camera_focalLength(double focalLength){
+    static char str[64];
+    static char num[64];
+    double_to_char(focalLength, str, 2);
+    strcat(str, "mm"); // "mm"を結合する
+    return MString(str);
+}
+
+// テキスト位置 左下
+MPoint get_viewport_leftBottom(int num, int width, int height, double fontSize, MPoint offset, MPoint margin, MPoint maskOffset) {
+    return MPoint(
+        offset.x + margin.x + maskOffset.x,
+        offset.y + maskOffset.y +(margin.y + fontSize) * num,
+        0);
+}
+
+// テキスト位置 左上
+MPoint get_viewport_leftTop(int num, int width, int height, double fontSize, MPoint offset, MPoint margin, MPoint maskOffset) {
+    return MPoint(
+        offset.x + margin.x + maskOffset.x,
+        height - maskOffset.y - offset.y - (margin.y + fontSize) * (num + 1),
+        0);
+}
+
+// テキスト位置 右下
+MPoint get_viewport_rightBottom(int num, int width, int height, double fontSize, MPoint offset, MPoint margin, MPoint maskOffset) {
+    return MPoint(
+        width - offset.x - margin.x - maskOffset.x,
+        offset.y + maskOffset.y + (margin.y + fontSize) * num,
+        0);
+}
+
+// テキスト位置 右上
+MPoint get_viewport_rightTop(int num, int width, int height, double fontSize, MPoint offset, MPoint margin, MPoint maskOffset) {
+    return MPoint(
+        width - offset.x - margin.x - maskOffset.x,
+        height - maskOffset.y - offset.y - (margin.y + fontSize) * (num + 1),
+        0);
+}
+
+// テキスト位置 真ん中下
+MPoint get_viewport_centerBottom(int num, int width, int height, double fontSize, MPoint offset, MPoint margin, MPoint maskOffset) {
+    return MPoint(
+        width / 2,
+        offset.y + maskOffset.y + (margin.y + fontSize) * num,
+        0);
+}
+
+// テキスト位置 真ん中上
+MPoint get_viewport_centerTop(int num, int width, int height, double fontSize, MPoint offset, MPoint margin, MPoint maskOffset) {
+    return MPoint(
+        width / 2,
+        height - maskOffset.y - offset.y - (margin.y + fontSize) * (num + 1),
+        0);
+}
+
+// カメラのレゾリューションゲートマスク取得と計算 ※Fillの場合
+MPoint get_camera_resolution_fill(
+    double overscan,
+    int viewport_width,
+    int viewport_height
+    ) {
+
+    double device_aspect_ratio = 1.778;
+    double viewport_aspect_ratio = static_cast<double>(viewport_width) / static_cast<double>(viewport_height);
+    double mask_width = 0.0;
+    double mask_height = 0.0;
+
+    //横に長く上下のゲートマスクが無い場合
+    if (viewport_aspect_ratio > device_aspect_ratio * overscan) { 
+        MGlobal::displayInfo(MString("if                                    :"));
+        mask_height = 0;
+        mask_width = (viewport_width - viewport_width / overscan) / 2;
+    }
+    else if (device_aspect_ratio * overscan > viewport_aspect_ratio && viewport_aspect_ratio > device_aspect_ratio / viewport_aspect_ratio / overscan * 2) { //上下のゲートマスク有り
+        MGlobal::displayInfo(MString("else if 01                                     :"));
+        mask_height = (viewport_height - viewport_height / overscan) / 2 * (device_aspect_ratio * overscan - viewport_aspect_ratio) * 2 ;
+        mask_width = (viewport_width - viewport_width / overscan) / 2;
+    }
+    else if ((device_aspect_ratio - (viewport_aspect_ratio / overscan * 2)) < 0) { //縦に長く
+        MGlobal::displayInfo(MString("else if 02                                     :"));
+        mask_height = (viewport_height - viewport_height / overscan) / 2 * device_aspect_ratio * device_aspect_ratio / (viewport_aspect_ratio / overscan * 2);
+        mask_width = (viewport_width - viewport_width / overscan) / 2 * std::pow(( (viewport_aspect_ratio / overscan * 2) - device_aspect_ratio) * overscan, 0.5 );
+    }
+
+    //縦に長く左右のゲートマスクが無い場合
+    else {
+        MGlobal::displayInfo(MString("else                                     :"));
+        mask_height = (viewport_height - viewport_height / overscan) / 2 * device_aspect_ratio;
+        mask_width = 0;
+    }
+
+    MGlobal::displayInfo(MString("viewport_aspect_ratio                                         :") + viewport_aspect_ratio);
+    MGlobal::displayInfo(MString("device_aspect_ratio                                           :") + device_aspect_ratio);
+    MGlobal::displayInfo(MString("viewport_aspect_ratio / overscan                              :") + (viewport_aspect_ratio / overscan));
+    MGlobal::displayInfo(MString("viewport_aspect_ratio / overscan * 2                          :") + (viewport_aspect_ratio / overscan * 2));
+    MGlobal::displayInfo(MString("device_aspect_ratio / viewport_aspect_ratio / overscan * 2    :") + (device_aspect_ratio /viewport_aspect_ratio / overscan * 2));
+    MGlobal::displayInfo(MString("device_aspect_ratio / overscan                                :") + (device_aspect_ratio / overscan));
+    MGlobal::displayInfo(MString("viewport_aspect_ratio * overscan                              :") + (viewport_aspect_ratio * overscan));
+    MGlobal::displayInfo(MString("device_aspect_ratio * overscan                                :") + (device_aspect_ratio * overscan));
+    MGlobal::displayInfo(MString("(device_aspect_ratio * overscan - viewport_aspect_ratio) * 2  :") + ((device_aspect_ratio * overscan - viewport_aspect_ratio) * 2));
+    MGlobal::displayInfo(MString(" ( (viewport_aspect_ratio / overscan * 2) - device_aspect_ratio) * overscan:") + ((viewport_aspect_ratio / overscan * 2) - device_aspect_ratio) * overscan);
+    MGlobal::displayInfo(MString("mask_height                                                   :") + mask_height);
+    MGlobal::displayInfo(MString("mask_width                                                    :") + mask_width);
+
+
+    return MPoint(mask_width, mask_height, 0.0);
+}
+
+// カメラのレゾリューションゲートマスク取得と計算 ※Fillの場合
+MPoint get_camera_resolution_horizontal(
+    double overscan,
+    int viewport_width,
+    int viewport_height
+) {
+
+    double device_aspect_ratio = 1.778;
+    double viewport_aspect_ratio = static_cast<double>(viewport_width) / static_cast<double>(viewport_height);
+    double mask_width = 0.0;
+    double mask_height = 0.0;
+
+    if (viewport_aspect_ratio > device_aspect_ratio * overscan) { //横に長く上下のゲートマスクが無い場合
+        mask_height = 0;
+        mask_width = (viewport_width - viewport_width / overscan) / 2;
+    }
+    else if (viewport_aspect_ratio < device_aspect_ratio * overscan) { //上下のゲートマスク有り
+        mask_height = (viewport_height - viewport_height / overscan) / 2 * (device_aspect_ratio * overscan - viewport_aspect_ratio) * 2;
+        mask_width = (viewport_width - viewport_width / overscan) / 2;
+    }
+
+    return MPoint(mask_width, mask_height, 0.0);
+}
+
+// filmFitを文字列に変換
+//void convert_unitToTime(const MTime::Unit& unit, double& framesPerSecond)
+MString getFilmFitAsString(const MFnCamera::FilmFit& filmFit)
+{
+    MGlobal::displayInfo("-----------------check-------------------");
+
+    MString filmFitString;
+    switch (filmFit)
+    {
+    case MFnCamera::kFillFilmFit:
+        filmFitString = "fill";
+        break;
+    case MFnCamera::kHorizontalFilmFit:
+        filmFitString = "horizontal";
+        break;
+    case MFnCamera::kVerticalFilmFit:
+        filmFitString = "vertical";
+        break;
+    case MFnCamera::kOverscanFilmFit:
+        filmFitString = "overscan";
+        break;
+    default:
+        MGlobal::displayError("Unknown film fit.");
+        return MString();
+        break;
+    }
+    MGlobal::displayInfo(MString("filmFitString:") + filmFitString);
+    return filmFitString;
+
+}
+
+// カメラキャッシュのアトリビュート状態を取得
+MString get_camera_cache(bool camera_cache) {
+    if (camera_cache) {
+        return MString("Cache : ON");
+    }
+    else {
+        return MString("Cache : OFF");
+    }
+}
